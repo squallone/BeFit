739773e1b10db314deac28a9a3fe8eeaf48b7ea2
Abdiel Soto <asoto@etsy.com>
2021-08-14 23:52:37 -0500

    Initial Commit

---
+++ Shared/BeFitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFitSwiftUIApp.swift
+//  Shared
+//
+//  Created by Abdiel Soto on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BeFitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
